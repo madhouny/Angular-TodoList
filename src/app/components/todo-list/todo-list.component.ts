36d3edc7d342cee31e40e444e2a079d63a930ba1
madhouny <younes.madhoun@gmail.com>
2020-11-20 20:38:12 +0100

    Create an interface Todo

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../interfaces/todo';
 
 @Component({
   selector: 'todo-list',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class TodoListComponent implements OnInit {
   todoTitle: string = '';
   idForTodo: number = 0;
-  todos : object[] = [];
+  todos : Todo[] = [];
   constructor() { }
 
   ngOnInit(): void {
@@ -60,3 +61,5 @@ export class TodoListComponent implements OnInit {
     this.todos = this.todos.filter(todo => todo.id !== id);
   }
 }
+
+
