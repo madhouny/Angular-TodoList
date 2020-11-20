24b5524ace32bc451c497608771a521c39b3c527
madhouny <younes.madhoun@gmail.com>
2020-11-20 19:59:52 +0100

    Add Todos using 2 way data binding and FormsModule

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -6,10 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
-
+  todoTitle: string = '';
+  todos : object[] = [];
   constructor() { }
 
   ngOnInit(): void {
+    this.todoTitle = '';
+    this.todos = [
+      {
+        'id': 1,
+        'title': 'Finish Angular Screencast',
+        'completed': false,
+        'editing':false 
+      },
+      {
+        'id': 2,
+        'title': 'Take over world',
+        'completed': false,
+        'editing':false 
+      },
+      {
+        'id': 3,
+        'title': 'One more thing',
+        'completed': false,
+        'editing':false 
+      },
+    ];
+  }
+
+  addTodo(){
+    this.todos.push({
+      id: 4,
+      title: this.todoTitle,
+      completed:false,
+      editing:false
+    })
   }
 
 }
