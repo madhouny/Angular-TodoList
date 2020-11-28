bc443f45582641e73b060e38e08a1bb390e28d1a
madhouny <younes.madhoun@gmail.com>
2020-11-28 22:50:26 +0100

    services and dependency injection to list-component & item-component

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,11 +1,13 @@
 import { animate, animation, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from 'src/app/services/todo.service';
 import { Todo } from '../../interfaces/todo';
 
 @Component({
   selector: 'todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss'],
+  providers:[TodoService],
   animations: [ 
     trigger('fade', [
 
@@ -23,39 +25,12 @@ import { Todo } from '../../interfaces/todo';
   ]
 })
 export class TodoListComponent implements OnInit {
-  todoTitle!: string;
-  idForTodo!: number;
-  todos!: Todo[];
-  beforeEditCache! :string;
-  filter!: string;
+   todoTitle!: string;
 
-  constructor() { }
+  constructor(public todoService: TodoService) { }
 
   ngOnInit(): void {
-    this.beforeEditCache = '';
-    this.idForTodo = 4;
-    this.todoTitle = '';
-    this.filter = 'all';
-    this.todos = [
-      {
-        'id': 1,
-        'title': 'Finish Angular Screencast',
-        'completed': false,
-        'editing':false 
-      },
-      {
-        'id': 2,
-        'title': 'Take over world',
-        'completed': false,
-        'editing':false 
-      },
-      {
-        'id': 3,
-        'title': 'One more thing',
-        'completed': false,
-        'editing':false 
-      },
-    ];
+     this.todoTitle = '';
   }
 
   //Adding todos
@@ -64,80 +39,9 @@ export class TodoListComponent implements OnInit {
       return;
     }
 
-    this.todos.push({
-      id: this.idForTodo,
-      title: this.todoTitle,
-      completed:false,
-      editing:false
-    })
-
-  //Clear input and increment IDS
+    this.todoService.addTodo(this.todoTitle);
     this.todoTitle = '';
-    this.idForTodo++;
-  
   }
-
-  // Edit Todo by Double Click
-    editTodo(todo: Todo) : void{
-      this.beforeEditCache = todo.title;
-      todo.editing = true;
-    }
-
-    doneEdit(todo:Todo):void{
-
-      //case Of empty String
-      if(todo.title.trim().length === 0){
-        todo.title = this.beforeEditCache;
-      }
-
-      todo.editing = false;
-    }
-
-    // Cancel edit when we tap Echap
-    cancelEdit(todo:Todo):void{
-      todo.title = this.beforeEditCache;
-      todo.editing = false;
-    }
-
-  //Delete Todos using filter function to create a new todos array without Id passed on parametre
-    deleteTodo(id: number): void{
-     this.todos = this.todos.filter(todo => todo.id !== id);
-    }
-
-  //update Items left
-    remaining(): number {
-      return this.todos.filter(todo => !todo.completed).length;
-    }
-
-  //Display or not Clear Completed button
-  atLeastOneItemCompleted() : boolean{
-    return this.todos.filter(todo => todo.completed).length> 0;
-  }
-
-  //Clear Completed button functionnality
-  clearCompleted() :void{
-    this.todos = this.todos.filter(todo => !todo.completed);
-  }
-
-  //Check All Items button functionnality
-  clearAllTodos() :void{
-    this.todos.forEach(todo => todo.completed = 
-      (<HTMLInputElement> event?.target).checked)
-  }
-
-  //Todos Filtered { all , active, completed}
-  todosFiltered(): Todo[]{
-    if(this.filter === 'all'){
-      return this.todos;
-    }else if(this.filter === 'active'){
-      return this.todos.filter(todo => !todo.completed)
-    }else if(this.filter === 'completed'){
-      return this.todos.filter(todo => todo.completed)
-    }
-
-    return this.todos;
-  }
-
 }
 
 
---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,119 @@
+import { Injectable } from '@angular/core';
+import { Todo } from '../interfaces/todo';
+
+@Injectable({ 
+  providedIn: 'root'
+})
+export class TodoService {
+
+  todoTitle: string='';
+  idForTodo: number= 4;
+  beforeEditCache :string = '';
+  filter: string = 'all';
+  anyRemainingModel: boolean = true;
+  todos: Todo[] = [
+    {
+      'id': 1,
+      'title': 'Finish Angular Screencast',
+      'completed': false,
+      'editing':false 
+    },
+    {
+      'id': 2,
+      'title': 'Take over world',
+      'completed': false,
+      'editing':false 
+    },
+    {
+      'id': 3,
+      'title': 'One more thing',
+      'completed': false,
+      'editing':false 
+    },
+  ];
+
+  constructor() { }
+
+  //Adding todos
+  addTodo(todoTitle:string): void{
+    if(todoTitle.trim().length === 0){
+      return;
+    }
+
+    this.todos.push({
+      id: this.idForTodo,
+      title: todoTitle,
+      completed:false,
+      editing:false
+    })
+
+  //Clear input and increment IDS
+    this.idForTodo++;
+  
+  }
+
+  // Edit Todo by Double Click
+    editTodo(todo: Todo) : void{
+      this.beforeEditCache = todo.title;
+      todo.editing = true;
+    }
+
+    doneEdit(todo:Todo):void{
+
+      //case Of empty String
+      if(todo.title.trim().length === 0){
+        todo.title = this.beforeEditCache;
+      }
+      this.anyRemainingModel = this.anyRemaining();
+      todo.editing = false;
+    }
+
+    // Cancel edit when we tap Echap
+    cancelEdit(todo:Todo):void{
+      todo.title = this.beforeEditCache;
+      todo.editing = false;
+    }
+
+  //Delete Todos using filter function to create a new todos array without Id passed on parametre
+    deleteTodo(id: number): void{
+     this.todos = this.todos.filter(todo => todo.id !== id);
+    }
+
+  //update Items left
+    remaining(): number {
+      return this.todos.filter(todo => !todo.completed).length;
+    }
+
+  //Display or not Clear Completed button
+  atLeastOneItemCompleted() : boolean{
+    return this.todos.filter(todo => todo.completed).length> 0;
+  }
+
+  //Clear Completed button functionnality
+  clearCompleted() :void{
+    this.todos = this.todos.filter(todo => !todo.completed);
+  }
+
+  //Check All Items button functionnality
+  clearAllTodos() :void{
+    this.todos.forEach(todo => todo.completed = 
+      (<HTMLInputElement> event?.target).checked)
+  }
+
+  anyRemaining(): boolean {
+    return this.remaining() !== 0;
+  }
+
+  //Todos Filtered { all , active, completed}
+  todosFiltered(): Todo[]{
+    if(this.filter === 'all'){
+      return this.todos;
+    }else if(this.filter === 'active'){
+      return this.todos.filter(todo => !todo.completed)
+    }else if(this.filter === 'completed'){
+      return this.todos.filter(todo => todo.completed)
+    }
+
+    return this.todos;
+  }
+}
