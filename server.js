17bfe50baed3e4ffa6a9b45b95e089af21c0bc9f
madhouny <younes.madhoun@gmail.com>
2020-12-13 12:40:56 +0100

    server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static(__dirname + 'todo-miashs'))
 app.get('/*', function(req,res){
-    res.sendFile(path.join(__dirname+ 'dist/index.html'));
+    res.sendFile(path.join(__dirname+ '/dist/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
